11ed6de04baa89dd49e4807646116138b447a932
Christopher Calle <christophercalle@gmail.com>
2018-06-02 00:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,4 @@ console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can\'t hear you!"}
+  if (string === string.toLowerCase()){
